5a018ef5b983b8405154feae14872296188543bb
Reuben Mathew <intelphysic@gmail.com>
2019-09-12 10:56:48 -0400

    lab2 complete

---
+++ EECS2030_Lab1/src/eecs2030/lab1/Hounsfield.java
@@ -0,0 +1,142 @@
+package eecs2030.lab1;
+
+/**
+ * A class that represents a Hounsfield unit. Hounsfield units are the units of
+ * measurement used in computed tomography (CT or CAT) scanning.
+ * 
+ * <p>
+ * The Hounsfield scale is defined by specifying the radiodensity of air as
+ * {@code -1000} Hounsfield units and the radiodensity of distilled water as
+ * {@code 0} Hounsfield units. Adjacent tissues in the human body can be
+ * distinguished from one another if their radiodensities differ; see
+ * <a href="https://en.wikipedia.org/wiki/Hounsfield_scale">the Wikipedia
+ * page</a> for a table of typical Hounsfield values for tissues of the human
+ * body.
+ * 
+ * <p>
+ * CT scanners for medical purposes typically restrict the value of reported
+ * Hounsfield units to integers in the range {@code -1024} to {@code 3071} so
+ * that a Hounsfield unit can be encoded as a 12-bit value. This class uses the
+ * values {@code -1024} and {@code 3071} to represent the minimum and maximum,
+ * respectively, allowable Hounsfield unit values.
+ *
+ */
+public class Hounsfield {
+
+	/**
+	 * The integer value of this Hounsfield unit
+	 */
+	private int value;
+
+	/**
+	 * The minimum Hounsfield unit reported by medical CT scanners Look up the
+	 * correct value in the documentation
+	 */
+	public static final int MIN_VALUE = -1024;
+
+	/**
+	 * The maximum Hounsfield unit reported by medical CT scanners Look up the
+	 * correct value in the documentation
+	 */
+	public static final int MAX_VALUE = 3072;
+
+	/**
+	 * Initializes this Hounsfield unit to have a value of zero.
+	 */
+	public Hounsfield() {
+		// A constructor should assign every non-static field a value.
+		// Change the next line to assign the correct value to this.value
+		// or use constructor chaining.
+		//
+		// Implement the get method before trying to run the JUnit test.
+		// If you used constructor chaining you will also have to implement
+		// the next constructor before running the JUnit test.
+		this.value = 0;
+	}
+
+	/**
+	 * Initializes this Hounsfield unit to have the specified value.
+	 * 
+	 * @param value the value of this Hounsfield unit
+	 * @throws IllegalArgumentException if {@code value} is less than the minimum
+	 *                                  Hounsfield unit reported by medical CT
+	 *                                  scanners or greater than the maximum
+	 *                                  Hounsfield unit reported by medical CT
+	 *                                  scanners
+	 */
+	public Hounsfield(int value) {
+		checkValue(value);
+		this.value = value;
+
+	}
+
+	/**
+	 * Initializes this Hounsfield unit by copying the value from the specified
+	 * other Hounsfield unit.
+	 * 
+	 * @param other the Hounsfield unit to copy the value from
+	 */
+	public Hounsfield(Hounsfield other) {
+		// Assign a value to this.value by copying other.value
+		this.value = other.value;
+	}
+
+	/**
+	 * Throws an {@code IllegalArgumentException} if the specified value is less
+	 * than the minimum Hounsfield unit reported by medical CT scanners or greater
+	 * than the maximum Hounsfield unit reported by medical CT scanners.
+	 * 
+	 * @param value a value to check
+	 * @throws IllegalArgumentException if the specified value is less than the
+	 *                                  minimum Hounsfield unit reported by medical
+	 *                                  CT scanners or greater than the maximum
+	 *                                  Hounsfield unit reported by medical CT
+	 *                                  scanners.
+	 */
+	private static void checkValue(int value) {
+		if ((value < MIN_VALUE) || (value > MAX_VALUE)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * Returns the value of this Hounsfield unit.
+	 * 
+	 * @return the value of this Hounsfield unit
+	 */
+	public int get() {
+		// Change the next line to return the value of this.value
+
+		return this.value;
+	}
+
+	/**
+	 * Sets the value of this Hounsfield unit to the specified value returning the
+	 * value that was overwritten.
+	 * 
+	 * @param value the value to set this Hounsfield unit to
+	 * @return the overwritten value of this Hounsfield unit
+	 * @throws IllegalArgumentException if the specified value is less than the
+	 *                                  minimum Hounsfield unit reported by medical
+	 *                                  CT scanners or greater than the maximum
+	 *                                  Hounsfield unit reported by medical CT
+	 *                                  scanners.
+	 */
+	public int set(int value) {
+		checkValue(value);
+		this.value = value;
+		return value;
+	}
+
+	/**
+	 * Returns a string representation of this Hounsfield unit. The returned string
+	 * is the numeric value of this Hounsfield unit (formatted as an integer)
+	 * followed by a space followed by the string {@code "HU"}.
+	 */
+	@Override
+	public String toString() {
+		// Change the next line to return the approriate string
+		String s = this.value + "HU";
+		return s;
+	}
+}
---
+++ EECS2030_Lab1/src/eecs2030/lab1/HounsfieldWindow.java
@@ -0,0 +1,93 @@
+package eecs2030.lab1;
+
+/**
+ * A class that represents a windowed view of Hounsfield units. A Hounsfield
+ * window is defined by two values: (1) the window level, and (2) the window
+ * width. The window level is the Hounsfield unit value that the window is
+ * centered on. The window width is the range of Hounsfield unit values that the
+ * window is focused on.
+ * 
+ * <p>
+ * A window has a lower and upper bound. The lower bound is defined as the
+ * window level minus half the window width:
+ * 
+ * <p>
+ * lo = level - (width / 2)
+ * 
+ * <p>
+ * The upper bound is defined as the window level plus half the window width:
+ * 
+ * <p>
+ * hi = level + (width / 2)
+ * 
+ * <p>
+ * Hounsfield units are mapped by the window to a real number in the range of
+ * {@code 0} to {@code 1}. A Hounsfield unit with a value less than lo is mapped
+ * to the value {@code 0}. A Hounsfield unit with a value greater than hi is
+ * mapped to the value {@code 1}. A Hounsfield unit with a value v between lo
+ * and hi is mapped to the value:
+ * 
+ * <p>
+ * (v - lo) / width
+ * 
+ *
+ */
+
+public class HounsfieldWindow {
+	public int level;
+	public int width;
+
+	public HounsfieldWindow() {
+		this.level = 0;
+		this.width = 400;
+	}
+
+	public HounsfieldWindow(int level, int width) {
+
+		if ((level < Hounsfield.MIN_VALUE) || (level > Hounsfield.MAX_VALUE))
+			throw new IllegalArgumentException();
+		else if (width < 1)
+			throw new IllegalArgumentException();
+
+		this.level = level;
+		this.width = width;
+
+	}
+
+	public int getWidth() {
+		return this.width;
+	}
+
+	public int getLevel() {
+		return this.level;
+	}
+
+	public int setLevel(int level) {
+		if ((level < Hounsfield.MIN_VALUE) || (level > Hounsfield.MAX_VALUE))
+			throw new IllegalArgumentException();
+		else
+			this.level = level;
+		return level;
+	}
+
+	public int setWidth(int width) {
+		if (width < 1)
+			throw new IllegalArgumentException();
+		else
+			this.width = width;
+		return width;
+	}
+
+	public double map(Hounsfield h) {
+		double v = (double) h.get();
+		double lo = this.level - (this.width / 2.0);
+		double hi = this.level + (this.width / 2.0);
+		if (v < lo) {
+			return 0;
+		} else if (v > hi) {
+			return 1;
+		} else {
+			return (v - lo) / this.width;
+		}
+	}
+}
