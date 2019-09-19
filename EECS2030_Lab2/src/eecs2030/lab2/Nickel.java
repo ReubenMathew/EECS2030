22d10a6fc9ade0cdbce3e2055adb7d6c314b9cfe
Reuben Mathew <intelphysic@gmail.com>
2019-09-19 10:25:57 -0400

    javadoc creation for nickel class

--- EECS2030_Lab2/src/eecs2030/lab2/Nickel.java
+++ EECS2030_Lab2/src/eecs2030/lab2/Nickel.java
@@ -1,14 +1,16 @@
 package eecs2030.lab2;
 
 public class Nickel implements Comparable<Nickel> {
+ 	
 
+	/** A class representing a Canadian nickel. The monetary value of nickel is defined to be five cents. Nickels were first minted in the year 1858. A nickel has an issue year which is the year in which the nickel was issued by the mint. */
 	private int year;
 
 	/**
 	 * The monetary value of a nickel in cents.
 	 */
 	public final int CENTS = 5;
-
+	
 	public Nickel(int year) {
 		if (year < 1858) {
 			throw new IllegalArgumentException();
